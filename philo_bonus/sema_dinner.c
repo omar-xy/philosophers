5cf781abefe21f1cc231b05cf14c977098d7076c
Omar Taraki <otaraki@e3r11p8.1337.ma>
2023-07-25 06:30:49 +0100

    MakefileAutoPush

--- philo_bonus/sema_dinner.c
+++ philo_bonus/sema_dinner.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 02:33:05 by otaraki           #+#    #+#             */
-/*   Updated: 2023/07/25 04:27:54 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/07/25 06:30:23 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,28 @@ void	routine(t_philo *ph)
 		ft_my_usleep(ph->table->time_to_eat, ph);
 		ph->last_meal = time_now();
 		unlock_forks(ph);
-		if (ph->table->nbr_meals != -1 && meal < ph->table->nbr_meals)
-			meal++;
-		if (meal == ph->table->nbr_meals)
-		{
-			sem_post(ph->table->nb_of_meals);
-			exit(0);	
-		}
+		// if (ph->table->nbr_meals != -1 && meal < ph->table->nbr_meals)
+		// 	meal++;
+		// if (meal == ph->table->nbr_meals)
+		// {
+		// 	sem_post(ph->table->nb_of_meals);
+		// 	exit(0);	
+		// }
 		sleeping(ph);
 		ft_my_usleep(ph->table->time_to_sleep, ph);
 		thinking(ph);
 	}
 }
-
+// should handle the case of one philo
+// should handle the optional case
 void	fork_for_philo(t_philo *ph, char **av)
 {
 	int		i;
-	pid_t		*pids;
+	pid_t	*pids;
 	pid_t	p;
 	t_philo	*tmp;
 
+	(void)av;
 	i = 0;
 	tmp = ph;
 	pids = malloc(sizeof(pid_t) * ph->table->nbr_of_philo);
@@ -66,16 +68,16 @@ void	fork_for_philo(t_philo *ph, char **av)
 	}
 	error:
 		free(pids);
-	if (av[5])
-	{
-		i = 0;
-		while (i < ph->table->nbr_of_philo)
-		{
-			sem_wait(ph->table->nb_of_meals);
-			i++;
-		}
-		sem_post(ph->table->death);
-	}
+	// if (av[5])
+	// {
+	// 	i = 0;
+	// 	while (i < ph->table->nbr_of_philo)
+	// 	{
+	// 		sem_wait(ph->table->nb_of_meals);
+	// 		i++;
+	// 	}
+	// 	sem_post(ph->table->death);
+	// }
 	sem_wait(ph->table->death);
 	i = 0;
 	while (i < ph->table->nbr_of_philo)
--- philo_bonus/time.c
+++ philo_bonus/time.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 02:24:42 by otaraki           #+#    #+#             */
-/*   Updated: 2023/07/25 03:45:51 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/07/25 06:22:56 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ void	ft_my_usleep(unsigned long t, t_philo *filo)
 	period = time_now();
 	while ((time_now() - period) < t)
     {
-	    usleep(100);
         if (time_now() - filo->last_meal > filo->table->time_to_die)
 		{
 			printf("%ld %d died \n",time_now() - (filo->table->time_begin), filo->id);
 			sem_post(filo->table->death);
             exit(0);
 		} 
+	    usleep(100);
     }
 	return ;
 }
